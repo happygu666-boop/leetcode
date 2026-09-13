ca5e20d9e7b77d893255d2c78eb22611c6103bcf
Happy <Happy.gu666@gmail.com>
2026-09-13 17:28:42 +0800

    002

---
+++ Answer001.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Answer001 {
+    public static void main(String[] args) {
+        Answer001 solution = new Answer001();
+        int[] nums = {2, 7, 11, 15};
+        int target = 17;
+        int[] result = solution.twoSum(nums, target);
+        System.out.println("Indices: " + result[0] + ", " + result[1]);
+    }
+
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> hashtable = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; ++i) {
+            if (hashtable.containsKey(target - nums[i])) {
+                return new int[]{hashtable.get(target - nums[i]), i};
+            }
+            hashtable.put(nums[i], i);
+        }
+        return new int[0];
+    }
+}
